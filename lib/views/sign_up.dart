2be52ec2e51fb6a9118d332baadad581a6580389
killian.zouareg <killian.zouareg@vadesecure.com>
2022-09-15 10:05:08 +0200

    Sign up function

--- lib/views/sign_up.dart
+++ lib/views/sign_up.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:veebz_front_flutter/views/profile_view.dart';
 import 'package:veebz_front_flutter/widgets/veebz_important_button.dart';
+import 'package:email_validator/email_validator.dart';
 
 import '../data/users.dart';
 import '../my_colors.dart';
@@ -15,18 +16,22 @@ class signUp extends StatefulWidget {
 
 class _signUpState extends State<signUp> {
   //Crete account function
-  Future<User?> createAccountUsingEmailPassword({required String email, required String password, required BuildContext context}) async{
-  FirebaseAuth auth = FirebaseAuth.instance;
-  User? user;
-  try {
-    UserCredential userCredential = await auth.createUserWithEmailAndPassword(email: email, password: password);
-    user = userCredential.user;
-  } on FirebaseAuthException catch (e){
-    print(e.message);
-  }
+  Future<User?> createAccountUsingEmailPassword(
+      {required String email,
+      required String password,
+      required BuildContext context}) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+    try {
+      UserCredential userCredential = await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      user = userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      print(e.message);
+    }
 
-  return user;
-}
+    return user;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +39,7 @@ class _signUpState extends State<signUp> {
     TextEditingController _userPassword = TextEditingController();
     TextEditingController _userPasswordConfirmed = TextEditingController();
     bool isPasswordMatched = false;
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: MyColors.BackgroundAppColor,
       body: SingleChildScrollView(
         physics: const ClampingScrollPhysics(),
@@ -52,33 +57,39 @@ class _signUpState extends State<signUp> {
                 height: MediaQuery.of(context).size.width / 2,
               ),
               SizedBox(
-                height: MediaQuery.of(context).size.height / 10,
+                height: MediaQuery.of(context).size.height / 20,
               ),
               const Text(
-                "Login Page",
+                "Sign Up Page",
                 style:
-                TextStyle(color: MyColors.NewPrimaryColor, fontSize: 30.0),
+                    TextStyle(color: MyColors.NewPrimaryColor, fontSize: 30.0),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 20,
               ),
               SizedBox(
                 height: MediaQuery.of(context).size.height / 30,
               ),
               TextField(
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.next,
+                autofocus: true,
                 controller: _userEmail,
                 maxLines: 1,
                 decoration: InputDecoration(
                     border: const OutlineInputBorder(
                         borderRadius: BorderRadius.all(Radius.circular(20))),
                     filled: true,
-                  fillColor: Colors.white.withOpacity(0.5),
+                    fillColor: Colors.white.withOpacity(0.5),
                     hintText: "Email",
-                    hintStyle: TextStyle(
-                        color: Colors.white,
-                        fontStyle: FontStyle.italic)),
+                    hintStyle: const TextStyle(
+                        color: Colors.white, fontStyle: FontStyle.italic)),
               ),
               SizedBox(
                 height: MediaQuery.of(context).size.height / 30,
               ),
               TextField(
+                textInputAction: TextInputAction.next,
                 obscureText: true,
                 controller: _userPassword,
                 decoration: InputDecoration(
@@ -87,9 +98,8 @@ class _signUpState extends State<signUp> {
                         borderRadius: BorderRadius.all(Radius.circular(20))),
                     filled: true,
                     hintText: "Password",
-                    hintStyle: TextStyle(
-                        color: Colors.white,
-                        fontStyle: FontStyle.italic)),
+                    hintStyle: const TextStyle(
+                        color: Colors.white, fontStyle: FontStyle.italic)),
               ),
               SizedBox(
                 height: MediaQuery.of(context).size.height / 30,
@@ -103,27 +113,28 @@ class _signUpState extends State<signUp> {
                         borderRadius: BorderRadius.all(Radius.circular(20))),
                     filled: true,
                     hintText: "Confirm Password",
-                    hintStyle: TextStyle(
-                        color: Colors.white,
-                        fontStyle: FontStyle.italic)),
+                    hintStyle: const TextStyle(
+                        color: Colors.white, fontStyle: FontStyle.italic)),
               ),
               SizedBox(
                 height: MediaQuery.of(context).size.height / 30,
               ),
-              VeebzImportantButton(text: "create account", onPressed: (
-                  ) async {
-                if (_userPassword.text == _userPasswordConfirmed.text){
-                  User? user = await createAccountUsingEmailPassword(
-                      email: _userEmail.text,
-                      password: _userPassword.text,
-                      context: context);
-                  print(user);
-                  if (user != null) {
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => ProfileView(
-                        user: Users.john)));
-                  }
-                }
-              }),
+              VeebzImportantButton(
+                  text: "create account",
+                  onPressed: () async {
+                    if (_userPassword.text == _userPasswordConfirmed.text) {
+                      User? user = await createAccountUsingEmailPassword(
+                          email: _userEmail.text,
+                          password: _userPassword.text,
+                          context: context);
+                      print(user);
+                      if (user != null) {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) =>
+                                ProfileView(user: Users.john)));
+                      }
+                    }
+                  }),
             ],
           ),
         ),
